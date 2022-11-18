29d855cca77724f90f56d46fc712b1a9ad4280b6
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-11-18 23:30:34 +0530

    :tada: complete next and prev evolution

--- src/Containers/DetailsShow/DetailsShow.jsx
+++ src/Containers/DetailsShow/DetailsShow.jsx
@@ -10,7 +10,7 @@ function DetailsShow({pokeData, apiData}) {
     const [prevEvo, setPrevEvo] = useState([]);
     const [row, setRow] = useState(pokeData);
 
-    console.log(row);
+    // console.log(row);
 
     useEffect(()=>{
       pokeData.hasOwnProperty('next_evolution') ? setNextEvo(pokeData.next_evolution) : setNextEvo([]);
@@ -21,8 +21,10 @@ function DetailsShow({pokeData, apiData}) {
     // console.log(prevEvo);
     const handleClick = (num)=>{
       var filteRow = apiData.filter((obj)=>obj.num == num);
-      console.log(num, filteRow );
+      // console.log(num, filteRow );
       setRow(filteRow[0]);
+      filteRow[0].hasOwnProperty('next_evolution') ? setNextEvo(filteRow[0].next_evolution) : setNextEvo([]);
+      filteRow[0].hasOwnProperty('prev_evolution') ? setPrevEvo(filteRow[0].prev_evolution) : setPrevEvo([]);
     }
 
   return (
@@ -32,27 +34,27 @@ function DetailsShow({pokeData, apiData}) {
             <div className={style.detailsContainer}>
                 <h4><b>ID: {row.num}</b></h4>
                 <p className={style.pokeName}><b>{row.name}</b></p> 
-                {/* <p className={style.pokeType}>Types: {row.type.map((typ)=>typ+"  ")}</p>
-                <p className={style.pokeWeak}>Weaknesses: {row.weaknesses.map((weak)=>weak+"  ")}</p> */}
+                <p className={style.pokeType}>Types: {row.type.map((typ)=>typ+"  ")}</p>
+                <p className={style.pokeWeak}>Weaknesses: {row.weaknesses.map((weak)=>weak+"  ")}</p>
                 <p className={style.pokeHw}>Height: {row.height}</p>
                 <p className={style.pokeHw}>Weight: {row.weight}</p>
 
                 <p className={style.pokeNp}>
-                  Next Evolution: {row.hasOwnProperty('next_evolution') ?  
-                  row.next_evolution.map((obj)=>{return "< "+obj.name}) : "****"}
-                </p>
+                Prev Evolution: {row.hasOwnProperty('prev_evolution') ? row.prev_evolution.map(obj=>"> "+obj.name) : "****"}
+                </p> 
 
                 <p className={style.pokeNp}>
-                Prev Evolution: {row.hasOwnProperty('prev_evolution') ? row.prev_evolution.map(obj=>"> "+obj.name) : "****"}
-                </p>  
-
-                {/* <p className={style.pokeNp}>
-                Next/Prev Evolution: {row.hasOwnProperty('next_evolution') ? row.hasOwnProperty('prev_evolution') ? row.prev_evolution[0].name : row.next_evolution[0].name : "no data"}
-                </p>  */}
+                  Next Evolution: {row.hasOwnProperty('next_evolution') ? row.next_evolution.map((obj)=>{return "< "+obj.name}) : "****"}
+                </p>
             </div>
-            <div>
+            <div className={style.btnContainer}>
+              {prevEvo.map(obj=>{
+                  return <button className={style.btn} variant="outlined" onClick={()=>handleClick(obj.num)}>
+                  {obj.name}
+                  </button>
+                })}
               {nextEvo.map(obj=>{
-                return <button variant="outlined" onClick={()=>handleClick(obj.num)}>
+                return <button className={style.btn} variant="outlined" onClick={()=>handleClick(obj.num)}>
                 {obj.name}
                 </button>
               })}
